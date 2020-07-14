a48252cd7bdc325d4ad7847a8841809bcfcf96ad
Cris Vizcaino <cris.vizcaino@live.com>
2020-07-14 06:24:36 +0000

    Added how to handle multiple errors

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ def sumAdd(num1, num2):
     return num1 + num2
   except TypeError as err:
     print(f'Please enter intergers for this function to work {err}')
-    
-   
+print(sumAdd(2,2))
 
-print(sumAdd(2,'2'))
+
+#handling mulitple errrors   
+def sumDiv(num1, num2):
+  try:
+    return num1/num2
+  except (TypeError, ZeroDivisionError) as err:
+    print(f'Please enter intergers for this function to work: ERROR: {err}')
+print(sumDiv(2,0))
