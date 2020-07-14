1c898de2eade238ef34a8aa611c54d8f9291cc59
Cris Vizcaino <cris.vizcaino@live.com>
2020-07-14 06:18:22 +0000

    cleaned the print statement to clear up the error message

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ def sumAdd(num1, num2):
   try:
     return num1 + num2
   except TypeError as err:
-    print("Please enter intergers for this function to work")
-    print(err)
+    print(f'Please enter intergers for this function to work {err}')
+    
    
 
-print(sumAdd('2',2))
+print(sumAdd(2,'2'))
